150784e2bb69f3cb8ac2f6014e2494a663603053
Stenin K George <steninkgeorge007@gmail.com>
2025-05-19 12:51:14 +0530

    buld

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { UserInitializationComponent } from "./initialize-user";
-import { Toaster } from "@/components/ui/sonner";
+// import { Toaster } from "@/components/ui/sonner";
 import { ToastProvider } from "./app-components/toast-provider";
 
 const geistSans = Geist({
